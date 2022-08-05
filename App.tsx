6d228ae2bf16d37f60664506aff4fadb87f053dc
FrozenKota <q38230kota@gmail.com>
2022-08-05 23:56:35 +0900

    case2: flex{flex, height}

--- App.tsx
+++ App.tsx
@@ -10,12 +10,14 @@ const App = () => {
         <View style={{flex:1, backgroundColor:'#FF0000'}}></View>
         <View style={{flex:1, backgroundColor:'#00FF00'}}></View>
         <View style={{flex:1, backgroundColor:'#0000FF'}}></View>
+        <View style={{flex:1, backgroundColor:'#AAAAAA'}}></View>
       </View>
       
       <View style={{flex:1, flexDirection: 'column'}}>
-        <View style={{flex:1, backgroundColor:'#AA0000'}}></View>
-        <View style={{flex:1, backgroundColor:'#00AA00'}}></View>
-        <View style={{flex:1, backgroundColor:'#0000AA'}}></View>
+        <View style={{height: height/4, backgroundColor:'#AA0000'}}></View>
+        <View style={{height: height/4, backgroundColor:'#00AA00'}}></View>
+        <View style={{height: height/4, backgroundColor:'#0000AA'}}></View>
+        <View style={{height: height/4, backgroundColor:'#AAAAAA'}}></View>
       </View>
      </View>
   )
