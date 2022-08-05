91d1bfb8d8a63f5879312a0568aa092cbc0be12e
FrozenKota <q38230kota@gmail.com>
2022-08-05 23:26:33 +0900

    case1: flex{ flex, flex}

--- App.tsx
+++ App.tsx
@@ -5,41 +5,24 @@ const { height, width } = Dimensions.get('window');
 
 const App = () => {
   return(
-    <View style={styles.mainContainer1}>
-      <View style={{flex:1, backgroundColor:'#FF0000'}}></View>
-      <View style={{flex:2, backgroundColor:'#00FF00'}}></View>
-      <View style={{flex:3, backgroundColor:'#0000FF'}}></View>
+    <View style={{flex:1, flexDirection:'row', height: height}}>
+      <View style={{flex:1, flexDirection:'column'}}>
+        <View style={{flex:1, backgroundColor:'#FF0000'}}></View>
+        <View style={{flex:1, backgroundColor:'#00FF00'}}></View>
+        <View style={{flex:1, backgroundColor:'#0000FF'}}></View>
+      </View>
       
-      {true && (<AbsoluteComponent /> )}
-    </View>
-  )
-}
-
-const AbsoluteComponent = () => {
-  return (
-    <View style={styles.mainContainer2}>
-      <View style={{height:height*(1/6), backgroundColor:'#AA0000'}}></View>
-      <View style={{height:height*(2/6), backgroundColor:'#00AA00'}}></View>
-      <View style={{height:height*(3/6), backgroundColor:'#0000AA'}}></View>
-    </View>
+      <View style={{flex:1, flexDirection: 'column'}}>
+        <View style={{flex:1, backgroundColor:'#AA0000'}}></View>
+        <View style={{flex:1, backgroundColor:'#00AA00'}}></View>
+        <View style={{flex:1, backgroundColor:'#0000AA'}}></View>
+      </View>
+     </View>
   )
 }
 
 export default App;
 
 const styles = StyleSheet.create({
-  mainContainer1: {
-    flex: 1,
-    flexDirection: 'column',
-    width: width,
-    backgroundColor: 'darkblue'
-  },
-  mainContainer2: {
-    position: 'absolute',
-    top: 0,
-    left:0,
-    width: width/2,
-    height: height,
-    backgroundColor: 'blue'
-  }
+
 });
