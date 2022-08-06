b56d94bb3cfbb46ee43642f0ab85b4f62a554ff4
FrozenKota <q38230kota@gmail.com>
2022-08-06 13:59:55 +0900

    case2: flex1:{ height/4 *4, height/4 *4}

--- App.tsx
+++ App.tsx
@@ -5,21 +5,22 @@ const { height, width } = Dimensions.get('window');
 
 const App = () => {
   return(
-    <View style={{flex:1, flexDirection:'row', height: height}}>
-      <View style={{flex:1, flexDirection:'column'}}>
-        <View style={{flex:1, backgroundColor:'#FF0000'}}></View>
-        <View style={{flex:1, backgroundColor:'#00FF00'}}></View>
-        <View style={{flex:1, backgroundColor:'#0000FF'}}></View>
-        <View style={{flex:1, backgroundColor:'#AAAAAA'}}></View>
+    
+    <View style={{flex: 1, flexDirection: 'row'}}>
+      <View style={{flex: 1, width: width/2}}>
+        <View style={{height: height/4, backgroundColor:'#FF0000'}}></View>
+        <View style={{height: height/4, backgroundColor:'#00FF00'}}></View>
+        <View style={{height: height/4, backgroundColor:'#0000FF'}}></View>
+        <View style={{height: height/4, backgroundColor:'#FFFFFF'}}></View>
       </View>
-      
-      <View style={{flex:1, flexDirection: 'column'}}>
+
+      <View style={{flex: 1, width: width/2}}>
         <View style={{height: height/4, backgroundColor:'#AA0000'}}></View>
         <View style={{height: height/4, backgroundColor:'#00AA00'}}></View>
         <View style={{height: height/4, backgroundColor:'#0000AA'}}></View>
         <View style={{height: height/4, backgroundColor:'#AAAAAA'}}></View>
       </View>
-     </View>
+    </View>
   )
 }
 
