17874128f584a339d249d1d4de9d1cde79e2b768
FrozenKota <q38230kota@gmail.com>
2022-08-06 17:20:22 +0900

    case3: heightからステータスバーの高さを引いたところ高さが一致した。解決

--- App.tsx
+++ App.tsx
@@ -1,24 +1,26 @@
 import React, {useState} from 'react';
-import { StyleSheet, Dimensions, View } from 'react-native';
+import { StyleSheet, StatusBar, Dimensions, View } from 'react-native';
 
 const { height, width } = Dimensions.get('window');
+const STATUSBAR_HEIGHT = StatusBar.currentHeight;
+const HEIGHT = height - (STATUSBAR_HEIGHT? STATUSBAR_HEIGHT : 0);
 
 const App = () => {
   return(
     
     <View style={{flex: 1, flexDirection: 'row'}}>
       <View style={{flex: 1, width: width/2}}>
-        <View style={{height: height/4, backgroundColor:'#FF0000'}}></View>
-        <View style={{height: height/4, backgroundColor:'#00FF00'}}></View>
-        <View style={{height: height/4, backgroundColor:'#0000FF'}}></View>
-        <View style={{height: height/4, backgroundColor:'#FFFFFF'}}></View>
+        <View style={{flex:1, backgroundColor:'#FF0000'}}></View>
+        <View style={{flex:1, backgroundColor:'#00FF00'}}></View>
+        <View style={{flex:1, backgroundColor:'#0000FF'}}></View>
+        <View style={{flex:1, backgroundColor:'#FFFFFF'}}></View>
       </View>
 
       <View style={{flex: 1, width: width/2}}>
-        <View style={{height: height/4, backgroundColor:'#AA0000'}}></View>
-        <View style={{height: height/4, backgroundColor:'#00AA00'}}></View>
-        <View style={{height: height/4, backgroundColor:'#0000AA'}}></View>
-        <View style={{height: height/4, backgroundColor:'#AAAAAA'}}></View>
+        <View style={{height: HEIGHT/4, backgroundColor:'#AA0000'}}></View>
+        <View style={{height: HEIGHT/4, backgroundColor:'#00AA00'}}></View>
+        <View style={{height: HEIGHT/4, backgroundColor:'#0000AA'}}></View>
+        <View style={{height: HEIGHT/4, backgroundColor:'#AAAAAA'}}></View>
       </View>
     </View>
   )
